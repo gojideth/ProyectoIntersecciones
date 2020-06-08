b975cbf49104dc8f390fd3d563a0c3bde1ef3053
Santiago <martinschiquilloc+2@gmail.com>
2020-06-07 23:18:00 -0500

    Agrego la clase contenedora de los circulos

---
+++ src/CirclesInArea.java
@@ -0,0 +1,18 @@
+public class CirclesInArea {
+    public final static int MAX = 2;
+    public Circle[] circles;
+
+    public CirclesInArea(Circle[] circles){
+        this.circles = circles;
+
+    }
+
+    public Circle[] getCircles() {
+        return circles;
+    }
+
+    public void setCircles(Circle[] circles) {
+        this.circles = circles;
+    }
+
+}
