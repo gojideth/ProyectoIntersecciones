6829cd944ccb75e2c4ca0acb546c781a1555ebcf
Santiago <martinschiquilloc+2@gmail.com>
2020-06-08 20:38:42 -0500

    Incluyo método para obtener intersecciones

--- src/CirclesInArea.java
+++ src/CirclesInArea.java
@@ -15,14 +15,14 @@ public class CirclesInArea {
     public void setCircles(Circle[] circles) {
         this.circles = circles;
     }
-    public void addCircle(Circle circle){
-        circles[circles.length -1] = circle;
-        resizeCirclesArray();
 
+    public void addCircle(Circle circle){
+        circles[circles.length - 1] = circle;
+        resizeCirclesArray();
     }
 
     public void resizeCirclesArray(){
-        Circle[] aux = new Circle[circles.length-+1];
+        Circle[] aux = new Circle[circles.length+1];
         for (int i = 0; i < circles.length; i++) {
             aux[i] = circles[i];
         }
@@ -37,10 +37,11 @@ public class CirclesInArea {
         }
         return  salida;
     }
+
     public double diferenceBetweenCoordinates(Circle circle1, Circle circle2){
          return (circle1.getPosition().distanceTo(circle2.getPosition()));
-
     }
+
     public double sumRadios(Circle circles[]){
         int i = 0;
         double salida = 0;
@@ -50,5 +51,17 @@ public class CirclesInArea {
         }
         return salida;
 
+    }
+
+    public int obtainIntersections(double distanceBetweenCoordinates, double sumRadios){
+        int count = 0;
+        for (int i = 0; i < circles.length-1 ; i++) {
+            for (int j = i + 1; j <circles.length-2 ; j++) {
+                if (distanceBetweenCoordinates > sumRadios) {
+                    count ++;
+                }
+            }
+        }
+        return count;
     }
 }
